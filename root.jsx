9a2f4872b96750b7f41e95374ab76e1d0120142e
alexpsimone <alex.p.simone@gmail.com>
2020-12-14 17:21:28 +0000

    added templated import script tags and ReactRouterDOM methods

---
+++ root.jsx
@@ -0,0 +1,10 @@
+const Router = ReactRouterDOM.BrowserRouter;
+const Route =  ReactRouterDOM.Route;
+const Link =  ReactRouterDOM.Link;
+const Prompt =  ReactRouterDOM.Prompt;
+const Switch = ReactRouterDOM.Switch;
+const Redirect = ReactRouterDOM.Redirect;
+const useParams = ReactRouterDOM.useParams;
+const useHistory = ReactRouterDOM.useHistory;
+// same as the above but using destructing syntax 
+// const { useHistory, useParams, Redirect, Switch, Prompt, Link, Route } = ReactRouterDOM;
