9f8cc7956bb45dc19aa72e8247e9f77a360f6843
alexpsimone <alex.p.simone@gmail.com>
2020-12-14 17:45:01 +0000

    added directories and basic structure to server.py to allow rendering of root route

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect, send_from_directory
+from flask import request, flash
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template('root.html')
+
+if __name__ == "__main__":
+    # connect_to_db(app)
+    app.run(debug=True, host="0.0.0.0")
