340c2852a1581a8ef3daee73ca037026f825f2a9
alexpsimone <alex.p.simone@gmail.com>
2020-12-15 00:04:13 +0000

    added background image and more bootstrap grid to home

--- static/root.jsx
+++ static/root.jsx
@@ -13,11 +13,43 @@ function Home() {
     return (
         <React.Fragment>
             <div class="home container-fluid">
-                <div class="row">
-                    <div class="col">
-                        <h1>Avocado Toast Manifesto</h1>
+                <section class="section-1 container-fluid d-flex justify-content-center align-items-center">
+                    <div class="row">
+                        <div class="col">
+                            <p class="display-4 mx-5">
+                                Avocado Toast Manifesto
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </section>
+                <section class="section-2 container-fluid d-flex justify-content-center align-items-center">
+                    <div class="row">
+                        <div class="col">
+                            <p class="display-4 mx-5">
+                                I'll put some welcome text here.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+                <section class="section-3 container-fluid d-flex justify-content-center align-items-center">
+                    <div class="row">
+                        <div class="col">
+                            <p class="display-4 mx-5">
+                                Thing1
+                            </p>
+                        </div>
+                        <div class="col">
+                            <p class="display-4 mx-5">
+                                Thing 2
+                            </p>
+                        </div>
+                        <div class="col">
+                            <p class="display-4 mx-5">
+                                Thing 3
+                            </p>
+                        </div>
+                    </div>
+                </section>
             </div>
         </React.Fragment>
     )
