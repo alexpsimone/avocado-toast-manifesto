83e8783423dbe8cb3fb86f5dbeaa1bc2df4fe1a5
alexpsimone <alex.p.simone@gmail.com>
2020-12-15 00:59:51 +0000

    added Manifesto React component

--- static/root.jsx
+++ static/root.jsx
@@ -9,44 +9,32 @@ const useHistory = ReactRouterDOM.useHistory;
 // same as the above but using destructing syntax 
 // const { useHistory, useParams, Redirect, Switch, Prompt, Link, Route } = ReactRouterDOM;
 
+function Manifesto(props) {
+    return (
+        <p> ~*~*~ {props.manifesto} ~*~*~ </p>
+    );
+}
+
 function Home() {
     return (
         <React.Fragment>
             <div class="home container-fluid">
                 <section class="section-1 container-fluid d-flex justify-content-center align-items-center">
-                    <div class="row">
-                        <div class="col">
-                            <p class="display-4 mx-5">
-                                Avocado Toast Manifesto
-                            </p>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col">
+                                <h1 class="display-4 mx-5">
+                                    Avocado Toast Manifesto
+                                </h1>
+                            </div>
                         </div>
                     </div>
                 </section>
                 <section class="section-2 container-fluid d-flex justify-content-center align-items-center">
                     <div class="row">
                         <div class="col">
-                            <p class="display-4 mx-5">
-                                I'll put some welcome text here.
-                            </p>
-                        </div>
-                    </div>
-                </section>
-                <section class="section-3 container-fluid d-flex justify-content-center align-items-center">
-                    <div class="row">
-                        <div class="col">
-                            <p class="display-4 mx-5">
-                                Thing1
-                            </p>
-                        </div>
-                        <div class="col">
-                            <p class="display-4 mx-5">
-                                Thing 2
-                            </p>
-                        </div>
-                        <div class="col">
-                            <p class="display-4 mx-5">
-                                Thing 3
-                            </p>
+                            <button class="btn btn-primary" id="manifesto">Generate a Manifesto.</button>
+                            <Manifesto manifesto=" "/>
                         </div>
                     </div>
                 </section>
