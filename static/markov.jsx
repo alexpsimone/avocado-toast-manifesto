7ae29ab4eed387561ef72baf7fd57deac7c1074c
alexpsimone <alex.p.simone@gmail.com>
2020-12-27 20:48:28 +0000

    added seed text, added return lines to jsx func

--- static/markov.jsx
+++ static/markov.jsx
@@ -1,37 +1,42 @@
 "use strict";
 
 function readText () {
- 
-    const fs = require('fs') 
 
-    fs.readFile('seed_text.txt', 'utf-8', (err, data) => { 
-        
-        if (err) throw err; 
-        
-        // TODO: remove tabs, carriage returns, etc.
-        const words = data.split(" ");  // Makes a list of all words in the seed file.
+    function makeChains() {    
+        const fs = require('fs') 
 
-        // Instantiate an empty hashmap.
-        let chains = new Map();
-
-        // Check the hashmap for words from the seed file.
-        // If the word is an existing key, then push the following word in the text to its values.
-        // Otherwise, add the new key with a value of the following word, in an array.
-        for (let idx = 0; idx < (words.length - 2); idx += 1) {
+            fs.readFile('seed_text.txt', 'utf-8', (err, data) => { 
             
-            let key = words[idx];
+                if (err) throw err; 
+                
+                // TODO: remove tabs, carriage returns, etc.
+                const words = data.split(" ");  // Makes a list of all words in the seed file.
 
-            if (chains.has(key)) {
+                // Instantiate an empty hashmap.
+                let chains = new Map();
 
-                chains.get(key).push(words[idx + 2]);
-            } else {
-                chains.set(key, [words[idx + 2]]);
-            }
-        };
+                // Check the hashmap for words from the seed file.
+                // If the word is an existing key, then push the following word in the text to its values.
+                // Otherwise, add the new key with a value of the following word, in an array.
+                for (let idx = 0; idx < (words.length - 2); idx += 1) {
+                    
+                    let key = words[idx];
 
-    });
+                    if (chains.has(key)) {
+
+                        chains.get(key).push(words[idx + 2]);
+                    } else {
+                        chains.set(key, [words[idx + 2]]);
+                    }
+                };
+
+                return chains;
+
+            });
+    }
+    return makeChains;
 
 };
 
 const chains = readText();
-console.log(chains);
+console.log(chains());
