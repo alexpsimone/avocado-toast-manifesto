c8e94aa60c431851b619f26ce41356d20a330bc8
alexpsimone <alex.p.simone@gmail.com>
2020-12-27 01:43:21 +0000

    populated hashmap with key-value pairs of words to chain together

--- static/markov.jsx
+++ static/markov.jsx
@@ -1,22 +1,37 @@
 "use strict";
 
-function openReadFile () {
+function readText () {
  
     const fs = require('fs') 
 
     fs.readFile('seed_text.txt', 'utf-8', (err, data) => { 
         
         if (err) throw err; 
-        console.log(data); 
-    }) ;
-}
+        
+        // TODO: remove tabs, carriage returns, etc.
+        const words = data.split(" ");  // Makes a list of all words in the seed file.
 
-openReadFile();
+        // Instantiate an empty hashmap.
+        let chains = new Map();
 
-function makeChains () {
+        // Check the hashmap for words from the seed file.
+        // If the word is an existing key, then push the following word in the text to its values.
+        // Otherwise, add the new key with a value of the following word, in an array.
+        for (let idx = 0; idx < (words.length - 2); idx += 1) {
+            
+            let key = words[idx];
 
-}
+            if (chains.has(key)) {
 
-function makeText () {
+                chains.get(key).push(words[idx + 2]);
+            } else {
+                chains.set(key, [words[idx + 2]]);
+            }
+        };
 
-}
+        console.log(chains);
+    });
+
+};
+
+readText();
